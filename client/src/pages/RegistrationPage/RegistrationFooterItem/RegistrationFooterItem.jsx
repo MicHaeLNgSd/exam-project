fc62a327e146d39c1fae1f7c9eee26dc86ed0834
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2024-06-26 09:55:58 +0300

    Change RegistrationFooterItem by deleting not needed spans and add link functionality for the future

--- client/src/pages/RegistrationPage/RegistrationFooterItem/RegistrationFooterItem.jsx
+++ client/src/pages/RegistrationPage/RegistrationFooterItem/RegistrationFooterItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from '../RegistrationFooter/RegistrationFooter.module.sass';
 
 function RegistrationFooterItem({ item }) {
@@ -6,13 +7,16 @@ function RegistrationFooterItem({ item }) {
 
   const getArticle = (article) => {
     if (typeof article === 'string') return article;
+    if (typeof article !== 'object') return null;
 
-    return article.map(({ text, span, end, link }, index) => (
+    return article.map(({ text, span, end, link = '#' }, index) => (
       <React.Fragment key={index}>
-        {text && <span>{text} </span>}
+        {text}{' '}
         {span && (
           <>
-            <span className={styles.orangeSpan}>{span}</span>
+            <Link to={link} className={styles.orangeSpan}>
+              {span}
+            </Link>
             {end}{' '}
           </>
         )}
