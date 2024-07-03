5b1f287743eb191dc8ced94bad1574cf39cdd43f
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2024-07-03 18:09:17 +0300

    Update ClickOpenElement styles and component + add all other questions

--- client/src/pages/HowItWorksPage/ClickOpenElement/ClickOpenElement.jsx
+++ client/src/pages/HowItWorksPage/ClickOpenElement/ClickOpenElement.jsx
@@ -1,9 +1,10 @@
 import React, { useRef, useState } from 'react';
 import styles from './ClickOpenElement.module.sass';
 import classNames from 'classnames';
+import { GoPlus } from 'react-icons/go';
 
 function ClickOpenElement({ data, imgPath }) {
-  const { question, answer } = data;
+  const { question, answer, list } = data;
   const [isOpen, setIsOpen] = useState(false);
   const contentRef = useRef(null);
   const clickHandler = () => {
@@ -14,21 +15,43 @@ function ClickOpenElement({ data, imgPath }) {
     [styles.active]: isOpen,
   });
 
-  const contentWrapperStyle = {
+  const contentContainerStyle = {
     maxHeight: isOpen ? contentRef.current.scrollHeight : 0,
+  };
+
+  const getList = () => {
+    if (!list) return null;
+    return (
+      <ul className={styles.list}>
+        {list.map((li, i) => (
+          <li
+            key={i}
+            dangerouslySetInnerHTML={{ __html: li }}
+            className={styles.listItem}
+          />
+        ))}
+      </ul>
+    );
   };
 
   return (
     <div className={containerClass}>
       <button className={styles.header} onClick={clickHandler}>
         {question}
+        <GoPlus className={styles.plusIcon} />
       </button>
       <div
-        className={styles.contentWrapper}
-        style={contentWrapperStyle}
+        className={styles.contentContainer}
+        style={contentContainerStyle}
         ref={contentRef}
       >
-        <p className={styles.text}>{answer}</p>
+        <div className={styles.contentWrapper}>
+          <p
+            className={styles.text}
+            dangerouslySetInnerHTML={{ __html: answer }}
+          />
+          {getList()}
+        </div>
       </div>
     </div>
   );
