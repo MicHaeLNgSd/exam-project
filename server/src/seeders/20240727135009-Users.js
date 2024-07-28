bb1fd197bc4722a863b6b8164cc7c48ad8254cd4
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2024-07-28 13:35:09 +0300

    Add moderator user and others as seeders

---
+++ server/src/seeders/20240727135009-Users.js
@@ -0,0 +1,39 @@
+'use strict';
+const bcrypt = require('bcrypt');
+const { MODERATOR, CREATOR, CUSTOMER, SALT_ROUNDS } = require('../constants');
+
+/** @type {import('sequelize-cli').Migration} */
+
+const userCreate = (name, role, body) => {
+  const email = `${name}@gmail.com`;
+  const initValues = {
+    firstName: name,
+    lastName: name,
+    displayName: name,
+    email,
+    password: bcrypt.hashSync(email, SALT_ROUNDS),
+    role,
+  };
+
+  return { ...initValues, ...body };
+};
+
+const defaultUsersArr = [
+  userCreate('buyer', CUSTOMER),
+  userCreate('creative', CREATOR),
+  userCreate('moderator', MODERATOR),
+];
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users', defaultUsersArr, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', {
+      email: {
+        [Sequelize.Op.in]: [...defaultUsersArr.map((u) => u.email)],
+      },
+    });
+  },
+};
