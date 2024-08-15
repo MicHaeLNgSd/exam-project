b1e3a05b10ac3c1cedaebbde53b595101a31e7fd
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2024-08-15 15:41:09 +0300

    Refactor OffersReviewPage

--- client/src/pages/OffersReviewPage/OffersReviewPage.jsx
+++ client/src/pages/OffersReviewPage/OffersReviewPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import Header from '../../components/Header/Header';
 import { useDispatch, useSelector } from 'react-redux';
 import { clearOffers, getOffers } from '../../store/slices/offersSlice';
@@ -19,52 +19,57 @@ function OffersReviewPage() {
     (state) => state.contestByIdStore
   );
 
-  const loadMore = (startFrom = 0, limit = 8) => {
-    dispatch(
-      getOffers({
-        limit,
-        offset: startFrom,
-        status: CONSTANTS.OFFER_STATUS_REVIEWING,
-      })
-    );
-  };
+  const loadMore = useCallback(
+    (startFrom = 0, limit = 8) => {
+      console.log(startFrom, limit);
+      dispatch(
+        getOffers({
+          limit,
+          offset: startFrom,
+          status: CONSTANTS.OFFER_STATUS_REVIEWING,
+        })
+      );
+    },
+    [dispatch]
+  );
 
-  const scrollHandler = () => {
-    if (
+  const scrollHandler = useCallback(() => {
+    const isAtScrollEnd =
       window.innerHeight + document.documentElement.scrollTop >=
-      document.documentElement.offsetHeight - 1
-    ) {
-      if (haveMore) {
-        loadMore(offers.length);
-      }
-    }
-  };
-
-  useEffect(() => {
-    if (offers.length > 0 && offers.length < 8 && haveMore) {
+      document.documentElement.offsetHeight - 1;
+    if (haveMore && isAtScrollEnd) {
       loadMore(offers.length);
     }
-  }, [offers, haveMore]); //, loadMore
+  }, [haveMore, offers, loadMore]);
 
   useEffect(() => {
     window.addEventListener('scroll', scrollHandler);
     return () => {
       window.removeEventListener('scroll', scrollHandler);
     };
-  }, [offers]);
+  }, [scrollHandler]);
 
   useEffect(() => {
-    dispatch(clearOffers());
+    if (offers.length > 0 && offers.length < 8 && haveMore) {
+      loadMore(offers.length);
+    }
+  }, [offers, haveMore, loadMore]);
+
+  const tryGetOffers = useCallback(() => {
     dispatch(getOffers({ limit: 8, status: CONSTANTS.OFFER_STATUS_REVIEWING }));
   }, [dispatch]);
 
-  const tryGetOffers = () => {
-    dispatch(getOffers({ limit: 8, status: CONSTANTS.OFFER_STATUS_REVIEWING }));
-  };
+  useEffect(() => {
+    dispatch(clearOffers());
+    tryGetOffers();
+  }, [dispatch, tryGetOffers]);
 
   const renderData = () => {
     if (error) return <TryAgain getData={tryGetOffers} />;
-    if (offers.length === 0 && !isFetching) return <p>No offers to review</p>;
+    if (offers.length === 0 && !isFetching)
+      return (
+        <div className={styles.notFound}>There is no offers at this moment</div>
+      );
     return <OffersReviewList offers={offers} />;
   };
 
