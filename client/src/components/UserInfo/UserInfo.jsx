e7aecdb3e8e03e48249c6990ce5be907ca2f60b3
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2024-08-16 23:50:37 +0300

    Fix avatar update

--- client/src/components/UserInfo/UserInfo.jsx
+++ client/src/components/UserInfo/UserInfo.jsx
@@ -9,7 +9,7 @@ import styles from './UserInfo.module.sass';
 const UserInfo = (props) => {
   const updateUserData = (values) => {
     const formData = new FormData();
-    formData.append('file', values.file);
+    formData.append('file', values.avatar);
     formData.append('firstName', values.firstName);
     formData.append('lastName', values.lastName);
     formData.append('displayName', values.displayName);
