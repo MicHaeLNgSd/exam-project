92aa71071f33d6090e1c1aa891b62d13e6df6b48
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2024-08-28 00:25:11 +0300

    Fix upload same image twice in ImageUpload

--- client/src/components/InputComponents/ImageUpload/ImageUpload.jsx
+++ client/src/components/InputComponents/ImageUpload/ImageUpload.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useField } from 'formik';
 
 const ImageUpload = ({ name, classes }) => {
   const [{ value, ...field }, , helpers] = useField(name);
   const { uploadContainer, inputContainer, imgStyle, error } = classes;
   const [imgSrc, setImgSrc] = useState(value);
+  const fileInputRef = useRef(null);
 
   const onChange = ({ target }) => {
     const file = target.files[0];
@@ -23,7 +24,12 @@ const ImageUpload = ({ name, classes }) => {
   };
 
   useEffect(() => {
-    if (!value) setImgSrc('');
+    if (!value) {
+      setImgSrc('');
+      if (fileInputRef.current) {
+        fileInputRef.current.value = null;
+      }
+    }
   }, [value]);
 
   return (
@@ -33,14 +39,16 @@ const ImageUpload = ({ name, classes }) => {
         <span className={error}>
           Support only images (*.png, *.gif, *.jpeg)
         </span>
-        <input
-          {...field}
-          id="fileInput"
-          type="file"
-          accept=".jpg, .png, .jpeg"
-          onChange={onChange}
-        />
-        <label htmlFor="fileInput">Chose file</label>
+        <label>
+          Chose file
+          <input
+            {...field}
+            ref={fileInputRef}
+            type="file"
+            accept=".jpg, .png, .jpeg"
+            onChange={onChange}
+          />
+        </label>
       </div>
     </div>
   );
