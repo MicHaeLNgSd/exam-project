dcb9be983bb12e6c30dea1a3c94c939831092319
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2024-09-05 16:31:08 +0300

    Fix eventsSlice reducers side effect

--- client/src/store/slices/eventsSlice.js
+++ client/src/store/slices/eventsSlice.js
@@ -1,22 +1,51 @@
 import { createSlice } from '@reduxjs/toolkit';
+import {
+  decorateAsyncThunk,
+  fulfilledReducer,
+  pendingReducer,
+  rejectedReducer,
+} from '../../utils/store';
 
 const EVENTS_SLICE_NAME = 'events';
 
 const initialState = {
   events: [],
+  isFetching: false,
+  error: null,
+};
+
+export const getEvents = decorateAsyncThunk({
+  key: `${EVENTS_SLICE_NAME}/getEvents`,
+  thunk: async () => {
+    const storageEvents = JSON.parse(localStorage.getItem('events'));
+    return storageEvents;
+  },
+});
+
+export const setEvents = decorateAsyncThunk({
+  key: `${EVENTS_SLICE_NAME}/setEvents`,
+  thunk: async (_, { getState }) => {
+    const { events } = getState().eventsStore;
+    localStorage.setItem('events', JSON.stringify(events));
+  },
+});
+
+const extraReducers = (builder) => {
+  builder.addCase(getEvents.pending, pendingReducer);
+  builder.addCase(getEvents.fulfilled, (state, { payload }) => {
+    state.events = payload ?? [];
+  });
+  builder.addCase(getEvents.rejected, rejectedReducer);
+
+  builder.addCase(setEvents.pending, pendingReducer);
+  builder.addCase(setEvents.fulfilled, fulfilledReducer);
+  builder.addCase(setEvents.rejected, rejectedReducer);
 };
 
 const eventsSlice = createSlice({
   name: EVENTS_SLICE_NAME,
   initialState,
   reducers: {
-    getEvents: (state) => {
-      const storageEvents = JSON.parse(localStorage.getItem('events'));
-      state.events = storageEvents ?? [];
-    },
-    setEvents: (state) => {
-      localStorage.setItem('events', JSON.stringify(state.events));
-    },
     clearEvents: () => initialState,
     addEvent: (state, { payload }) => {
       payload.createdAt = new Date().toISOString();
@@ -29,17 +58,11 @@ const eventsSlice = createSlice({
       state.events = state.events.filter((e) => e.createdAt !== payload);
     },
   },
+  extraReducers,
 });
 
 const { actions, reducer } = eventsSlice;
 
-export const {
-  getEvents,
-  setEvents,
-  addEvent,
-  removeEvent,
-  clearEvents,
-  deleteEvent,
-} = actions;
+export const { addEvent, removeEvent, clearEvents, deleteEvent } = actions;
 
 export default reducer;
